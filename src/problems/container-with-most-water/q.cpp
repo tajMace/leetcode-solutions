5ab47012c9f253c663e0cfcc46bf106a0aaa887e
Taj Macé <taj.mac101@gmail.com>
2026-09-03 22:02:08 +1000

    challenge 11: cpp

--- src/problems/container-with-most-water/q.cpp
+++ src/problems/container-with-most-water/q.cpp
@@ -4,10 +4,22 @@
 
 /* ---------- SOLUTION START ---------- */
 
+using namespace std;
+#include <vector>
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
+        int ret = 0;
+        int leftPtr = 0;
+        int rightPtr = height.size() - 1;
+
+        while (leftPtr < rightPtr) {
+            int currVolume = min(height[leftPtr], height[rightPtr]) * (rightPtr - leftPtr);
+            ret = max(ret, currVolume);
+            (height[leftPtr] < height[rightPtr]) ? leftPtr++ : rightPtr--;
+        }
+
+        return ret;
     }
 };
-
